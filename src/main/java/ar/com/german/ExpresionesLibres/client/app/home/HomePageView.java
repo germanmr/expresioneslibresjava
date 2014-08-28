41589c6ff161898ebb646118c48ec3f7a707477c
German Muñoz <german.munioz.romano@gmail>
2014-08-28 20:59:15 -0300

    Agregados metodos para armar la condicion

--- src/main/java/ar/com/german/ExpresionesLibres/client/app/home/HomePageView.java
+++ src/main/java/ar/com/german/ExpresionesLibres/client/app/home/HomePageView.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import ar.com.german.ExpresionesLibres.shared.modelo.Regla;
+import ar.com.german.ExpresionesLibres.shared.modelo.TieneConceptoConValor;
 
+import com.google.gwt.cell.client.ButtonCell;
+import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.SelectionCell;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.SuggestBox;
@@ -26,6 +29,8 @@ public class HomePageView extends ViewImpl implements HomePagePresenter.MyView {
 
 	interface Binder extends UiBinder<Widget, HomePageView> {
 	}
+
+	ListDataProvider<Expresion> dataProvider;
 
 	@UiField
 	Button btnAgregarCondicion;
@@ -45,34 +50,37 @@ public class HomePageView extends ViewImpl implements HomePagePresenter.MyView {
 	@UiField
 	TextBox valor;
 
+	@UiField
+	TextArea condicionEscrita;
+
 	@Inject
 	HomePageView(Binder uiBinder) {
 
 		// Creo los operadores de concatenacion
-		MultiWordSuggestOracle oraculoOperadoresComprarcion = new MultiWordSuggestOracle();
+		MultiWordSuggestOracle oraculoOperadoresComparacion = new MultiWordSuggestOracle();
 
-		oraculoOperadoresComprarcion.add("ES IGUAL A ");
-		oraculoOperadoresComprarcion.add("ES DISTINTO DE ");
-		oraculoOperadoresComprarcion.add("ES MENOR QUE ");
-		oraculoOperadoresComprarcion.add("ES MENOR IGUAL QUE");
-		oraculoOperadoresComprarcion.add("ES MAYOR QUE ");
-		oraculoOperadoresComprarcion.add("ES MAYOR IGUAL QUE ");
-		oraculoOperadoresComprarcion.add("ESTA ENTRE");
+		oraculoOperadoresComparacion.add("ES IGUAL A "); // .equals() o ==
+		oraculoOperadoresComparacion.add("ES DISTINTO DE ");// <> o !.equals
+		oraculoOperadoresComparacion.add("ES MENOR QUE "); // <
+		oraculoOperadoresComparacion.add("ES MENOR IGUAL QUE"); // <=
+		oraculoOperadoresComparacion.add("ES MAYOR QUE "); // >
+		oraculoOperadoresComparacion.add("ES MAYOR IGUAL QUE "); // >=
+		oraculoOperadoresComparacion.add("ESTA ENTRE"); // .contains() para
+														// numeros y cadenas
 
-		operadoresComparacion = new SuggestBox(oraculoOperadoresComprarcion);
+		operadoresComparacion = new SuggestBox(oraculoOperadoresComparacion);
 
 		// Creo los operadores de concatenacion
 		MultiWordSuggestOracle oraculoConceptos = new MultiWordSuggestOracle();
 
-		oraculoConceptos.add("Prestacion");
-		oraculoConceptos.add("Obra Social");
-		oraculoConceptos.add("Especialidad Efector");
-		oraculoConceptos.add("Especialidad Efector");
-		oraculoConceptos.add("Convenio");
-		oraculoConceptos.add("Profesion del Prestador");
-		oraculoConceptos.add("Matricula del Prestador");
-		oraculoConceptos.add("Plan del Afiliado");
-		oraculoConceptos.add("Afiliado Gravado en IVA");
+		oraculoConceptos.add("Prestacion");// Prestacion
+		oraculoConceptos.add("Obra Social"); // Obra Social
+		oraculoConceptos.add("Especialidad Efector"); // EspecialidadEfector
+		oraculoConceptos.add("Convenio"); // Convenio
+		oraculoConceptos.add("Profesion del Prestador"); // ProfesionDelPrestador
+		oraculoConceptos.add("Matricula del Prestador"); // MatriculaDelPrestador
+		oraculoConceptos.add("Plan del Afiliado"); // PlanDelAfiliado
+		oraculoConceptos.add("Afiliado Gravado en IVA"); // AfiliadoGravadoIVA
 
 		conceptos = new SuggestBox(oraculoConceptos);
 
@@ -94,8 +102,28 @@ public class HomePageView extends ViewImpl implements HomePagePresenter.MyView {
 			}
 		});
 
+		ButtonCell buttonCell = new ButtonCell();
+		Column buttonColumn = new Column<Expresion, String>(buttonCell) {
+			@Override
+			public String getValue(Expresion object) {
+				// The value to display in the button.
+				// return object;
+				return "Eliminar condicion";
+			}
+		};
+		expresionesSimples.addColumn(buttonColumn, "Action");
+
+		// You can then set a FieldUpdater on the Column to be notified of
+		// clicks.
+		buttonColumn.setFieldUpdater(new FieldUpdater<Expresion, String>() {
+			public void update(int index, Expresion object, String value) {
+				// Value is the button value. Object is the row object.
+				Window.alert("You clicked: " + value);
+			}
+		});
+
 		// Create a data provider.
-		ListDataProvider<Expresion> dataProvider = new ListDataProvider<Expresion>();
+		dataProvider = new ListDataProvider<Expresion>();
 
 		// Connect the table to the data provider.
 		dataProvider.addDataDisplay(expresionesSimples);
@@ -118,6 +146,131 @@ public class HomePageView extends ViewImpl implements HomePagePresenter.MyView {
 	@Override
 	public void asignarFoco() {
 		conceptos.setFocus(true);
+	}
+
+	private void agregarCondicion() {
+
+		String condicion = "";
+		String concatenador = "";
+
+		// Agrego la expresion a la lista
+		Expresion expresion = new Expresion(condicion, concatenador);
+
+		dataProvider.getList().add(expresion);
+
+		/**
+		 * Inicializo para agregar una nueva condicion Concepto/ comparador /
+		 * Constantes / Regla Escrita
+		 */
+		// THISFORM.edtCondicion.Value=""
+		// thisform.cmbconcepto.DisplayValue=""
+		// .concepto=.F.
+		// thisform.cmboperadores.DisplayValue=""
+		// .operador=.F.
+		// thisform.edtlistaConstantes.Value=""
+		// .constante=.F.
+		// .concatenador=.F.
+		//
+
+		/**
+		 * Recorro la coleccion de ( expresion / concatenador) y vuelvo a
+		 * escribir la regla +
+		 * " Si concepto comparador constante/s concatenador "
+		 */
+
+		// * Tengo que agregar cada regla
+		// * oRegla()
+		// *'Concepto = "420101" AND [2] = 220'
+		// * Expresion concatenador: [1] = "420101" AND
+		// *aReglas(1)=CREATEOBJECT("Regla",'[1] = "420101" AND [2] = 220',159)
+		// ENDWITH
+
+	}
+
+	private void armarFuncion(TieneConceptoConValor element) {
+
+		/**
+		 * Para verificar que se cumple la expresion
+		 */
+
+		// DIMENSION aMapaValores(4)
+		// aMapaValores(1)=CREATEOBJECT("MapElement","Concepto","Comparador")
+		// aMapaValores(2)=CREATEOBJECT("MapElement","Comparador","Constante")
+		// aMapaValores(3)=CREATEOBJECT("MapElement","Constante","Concatenador")
+		// aMapaValores(4)=CREATEOBJECT("MapElement","Concatenador","Concepto")
+		/**
+		 * Tiene que cumplir con la siguiente regla: Expresion [Concatenador
+		 * Expresion][Concatenador Expresion]... Donde expresion: Concepto
+		 * Comparador Constante Concepto Comparador Constante Concatenador
+		 * Concepto ...
+		 */
+
+		// DO CASE
+		// CASE pElemento.tipo="Concepto"
+		// .expresionSimple.Concepto=pElemento
+		//
+		// CASE pElemento.tipo="Comparador"
+		// .expresionSimple.operador=pElemento
+		//
+		// CASE pElemento.tipo="Constante"
+		// .expresionSimple.operador=pElemento
+		// * Tengo que formatear segun el tipo de dato elegido
+		//
+		// CASE pElemento.tipo="Concatenador"
+		//
+		// OTHERWISE
+		// * y aca que mierda hago???
+		//
+		// ENDCASE
+
+		// IF RECCOUNT("cExpresion")>0 THEN
+		//
+		//
+		// SELECT cExpresion
+		// GO TOP
+		// lUltimoTipo=ALLTRIM(cExpresion.tipo)
+		// FOR EACH mapElement IN aMapaValores
+		// IF ALLTRIM(lUltimoTipo)=mapElement.clave THEN
+		// IF pElemento.tipo<>mapElement.valor THEN
+		// RETURN .F.
+		// ENDIF
+		//
+		// ENDIF
+		// ENDFOR
+		// ENDIF
+
+		// INSERT INTO cExpresion(tipo,codigo,valor)
+		// VALUES(pElemento.tipo,pElemento.codigo,pElemento.valor)
+
+		// Expresion expresion =new Expresion(new condi, concatenador);
+
+		// *.dibujarExpression()???
+		//
+		// SELECT cExpresion
+		// .EdtExpression.VALUE=""
+
+		// SCAN
+		// .EdtExpression.VALUE=.EdtExpression.VALUE+" " +
+		// ALLTRIM(cExpresion.valor)
+		// ENDSCAN
+		//
+		//
+		//
+		// ENDWITH
+
+	}
+
+	private void dibujarcondicion() {
+		// lConstante=""
+
+		// IF !ISBLANK(ALLTRIM(thisform.edtlistaConstantes.Value)) THEN
+		// lConstante="("+ALLTRIM(thisform.edtlistaConstantes.Value)+")"
+		// ENDIF
+
+		// THISFORM.edtCondicion.Value= "Si " +
+		// ALLTRIM(THISFORM.CMBconcepto.DisplayValue) + " " +
+		// ALLTRIM(thisform.cmbOperadores.DisplayValue) + " " + lConstante
+
 	}
 
 }
